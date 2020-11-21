22efa2e919ffe4c09f788505281673162d046f4e
Ben Weedon <ben@weedon.email>
2020-11-20 20:56:52 -0800

    Add swap.ps1 helper

---
+++ swap.ps1
@@ -0,0 +1,14 @@
+[CmdletBinding()]
+param(
+    [Parameter(Mandatory)]
+    [String] $Target
+)
+
+Push-Location $PSScriptRoot
+try {
+    Write-Host "Swapping $Target..."
+    cargo run -p sexe -- swap $Target .\target\debug\sexe_loader.exe
+    Write-Host "Done."
+} finally {
+    Pop-Location
+}
