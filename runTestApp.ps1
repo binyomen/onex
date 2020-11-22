b87f042443dbbbb418bd188f8b1ff8da2fdf6155
Ben Weedon <ben@weedon.email>
2020-11-22 14:46:24 -0800

    Add GitHub Actions validation

--- runTestApp.ps1
+++ runTestApp.ps1
@@ -1,3 +1,5 @@
+$local:ErrorActionPreference = 'Stop'
+
 Push-Location $PSScriptRoot
 try {
     if (Test-Path .\target\testapp) {
@@ -8,23 +10,33 @@ try {
     }
 
     cargo build
+    if (-not $?) { Write-Error 'Command failed' }
     cargo clippy --all-targets --all-features -- -D warnings
+    if (-not $?) { Write-Error 'Command failed' }
     mkdir .\target\testapp > $null
 
     Copy-Item -Recurse .\testapp\assets\* .\target\testapp
     Copy-Item .\target\debug\testapp.exe .\target\testapp
 
     cargo run -p sexe -- pack .\target\debug\sexe_loader.exe .\target\testapp .\target\testapp_packaged.exe
+    if (-not $?) { Write-Error 'Command failed' }
     .\target\testapp_packaged.exe arg1 arg2 arg3
+    if (-not $?) { Write-Error 'Command failed' }
 
     cargo run -p sexe -- swap .\target\testapp_packaged.exe .\target\debug\sexe_loader.exe .\target\testapp_packaged.exe
+    if (-not $?) { Write-Error 'Command failed' }
     .\target\testapp_packaged.exe arg1 arg2 arg3
+    if (-not $?) { Write-Error 'Command failed' }
     cargo run -p sexe -- swap .\target\testapp_packaged.exe .\target\debug\sexe_loader.exe
+    if (-not $?) { Write-Error 'Command failed' }
     .\target\testapp_packaged.exe arg1 arg2 arg3
+    if (-not $?) { Write-Error 'Command failed' }
 
     cargo run -p sexe -- list .\target\testapp_packaged.exe
+    if (-not $?) { Write-Error 'Command failed' }
 
     cargo run -p sexe -- extract .\target\testapp_packaged.exe .\target\extracted
+    if (-not $?) { Write-Error 'Command failed' }
     Get-ChildItem -Recurse .\target\extracted
 } finally {
     Pop-Location
