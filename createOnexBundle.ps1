facf3291e136dd45bf0a0c46143ab346fac8406f
Ben Weedon <ben@weedon.email>
2020-12-01 00:57:27 -0800

    Only package arm64 onex if a flag is specified
    This speeds up the dev inner loop two ways. First, it means you aren't
    building arm when you don't want to. Second, building with a new target
    seems to entirely invalidate the build cache, meaning you are
    effectively doing a clean build every time you run createOnexBundle.ps1.

--- createOnexBundle.ps1
+++ createOnexBundle.ps1
@@ -1,3 +1,8 @@
+[CmdletBinding()]
+param(
+    [Switch] $BuildArm
+)
+
 $local:ErrorActionPreference = 'Stop'
 
 Push-Location $PSScriptRoot
@@ -12,22 +17,28 @@ try {
     mkdir .\target\onex_bundle_output > $null
 
     cargo build --release
-    cargo build --release --target=aarch64-pc-windows-msvc
-
     if (-not $?) { Write-Error 'x64 build failed' }
+    if ($BuildArm) {
+        cargo build --release --target=aarch64-pc-windows-msvc
+        if (-not $?) { Write-Error 'arm64 build failed' }
+    }
+
     Copy-Item .\target\release\onex_loader.exe .\target\onex_bundle\onex_loader_x64.exe
-    if (-not $?) { Write-Error 'arm64 build failed' }
-    Copy-Item .\target\aarch64-pc-windows-msvc\release\onex_loader.exe .\target\onex_bundle\onex_loader_arm64.exe
+    if ($BuildArm) {
+        Copy-Item .\target\aarch64-pc-windows-msvc\release\onex_loader.exe .\target\onex_bundle\onex_loader_arm64.exe
+    }
 
     Copy-Item .\target\release\onex.exe .\target\onex_bundle
     Write-Output 'onex.exe' > .\target\onex_bundle\onex_run
     .\target\release\onex.exe pack .\target\onex_bundle .\target\onex_bundle_output\onex_x64.exe --loader .\target\onex_bundle\onex_loader_x64.exe
     if (-not $?) { Write-Error 'x64 packaging failed' }
 
-    Copy-Item .\target\aarch64-pc-windows-msvc\release\onex.exe .\target\onex_bundle
-    Write-Output 'onex.exe' > .\target\onex_bundle\onex_run
-    .\target\release\onex.exe pack .\target\onex_bundle .\target\onex_bundle_output\onex_arm64.exe --loader .\target\onex_bundle\onex_loader_arm64.exe
-    if (-not $?) { Write-Error 'arm64 packaging failed' }
+    if ($BuildArm) {
+        Copy-Item .\target\aarch64-pc-windows-msvc\release\onex.exe .\target\onex_bundle
+        Write-Output 'onex.exe' > .\target\onex_bundle\onex_run
+        .\target\release\onex.exe pack .\target\onex_bundle .\target\onex_bundle_output\onex_arm64.exe --loader .\target\onex_bundle\onex_loader_arm64.exe
+        if (-not $?) { Write-Error 'arm64 packaging failed' }
+    }
 } finally {
     Pop-Location
 }
